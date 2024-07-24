dabdf11ecff1a8b2b5b62a9e6d32c5cff5831c0c
Emre Akay <emre.akayy@hotmail.com>
2024-07-25 00:57:27 +0300

    2nd code has been added for Turtle Shape

--- Catch the Turtle.py
+++ Catch the Turtle.py
@@ -5,3 +5,11 @@ s = turtle.Screen()
 s.screensize(400, 400)
 s.title("Eat The Turtle")
 s.bgcolor("black")
+
+
+t = turtle.Turtle()
+t.color("#E6F404")
+t.shape("triangle")
+t.shapesize(1)
+t.penup()
+t.speed(10)
